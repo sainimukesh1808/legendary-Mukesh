f20db3be26a615020b2490c7664b662eb27189a4
SAINI Mukesh <MSI26@dsone.3ds.com>
2020-10-26 18:59:40 +0530

    Assignments from Team Adidas

---
+++ Assignments/DemoBlaze/code/DemoBlaze.java
@@ -0,0 +1,153 @@
+package TestNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DemoBlaze {
+	public static WebDriver driver;
+	public static Properties prop;
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 10;
+	public static WebDriverWait wait;
+	public DemoBlaze(){
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream("D:\\CIT\\eclipse\\workspace\\SeleniumSession\\src\\TestNG\\data.properties");
+			prop.load(ip);
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	@BeforeClass
+	public static void beforeClass(){
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\msi26.DSONE\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver,30);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		}
+	
+	public String readProperty(String object_key){
+		String object_value = prop.getProperty(object_key);
+		return object_value;
+	}
+	
+	public void addLaptop(String model){
+		  driver.get(readProperty("url"));
+		  driver.findElement(By.xpath(readProperty("laptop"))).click();
+		  wait.until(ExpectedConditions.elementToBeClickable(By.xpath(readProperty(model))));
+		  driver.findElement(By.xpath(readProperty(model))).click();
+		  wait.until(ExpectedConditions.elementToBeClickable(By.xpath(readProperty("add_to_cart")))); 
+		  driver.findElement(By.xpath(readProperty("add_to_cart"))).click();
+		  wait.until(ExpectedConditions.alertIsPresent());
+		  driver.switchTo().alert().accept();
+	  }
+	
+	  @Test(priority = 1)
+	  public void addSonyVaio() throws InterruptedException {
+		  addLaptop("sony_vaio_i5");
+	  }
+	  @Test(priority = 2)
+	  public void addDell() throws InterruptedException {
+		  addLaptop("dell_i7_8gb");
+	  }
+	  @Test(priority = 3)
+	  public void deleteDell() throws InterruptedException {
+		  driver.navigate().to(readProperty("url"));
+		  driver.findElement(By.xpath(readProperty("cart"))).click();
+		  String dell_model = readProperty("dell_model");
+		  String table = readProperty("table");
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(table))); 
+		  String all_rows = table + "/tbody/tr";
+		  List<WebElement> rows = driver.findElements(By.xpath(all_rows));
+		  for(int row=0; row<rows.size(); row++){
+			  String all_cols = all_rows + "[" + (row+1) + "]"+ "/td";
+			  List<WebElement> cols = driver.findElements(By.xpath(all_cols));
+			  for(int col=0; col<cols.size(); col++){
+				  if(dell_model.equals(cols.get(col).getText())){
+					  driver.findElement(By.xpath(all_cols + "/a" + "[1]")).click();  
+					  break;
+				  }
+			  }
+			  
+		  }
+	  }
+	  
+	  @Test(priority = 4)
+	  public void placeOrder() throws InterruptedException {
+		  Thread.sleep(5000);
+		  wait.until(ExpectedConditions.elementToBeClickable(By.xpath(readProperty("place_order")))); 
+		  driver.findElement(By.xpath(readProperty("place_order"))).click();
+		  
+	  }
+	  
+	  @Test(priority = 5)
+	  public void fillForm() throws InterruptedException {
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(readProperty("name")))); 
+		  driver.findElement(By.xpath(readProperty("name"))).sendKeys("Mukesh");
+		  driver.findElement(By.xpath(readProperty("country"))).sendKeys("India");
+		  driver.findElement(By.xpath(readProperty("city"))).sendKeys("Jaipur");
+		  driver.findElement(By.xpath(readProperty("card"))).sendKeys("ICICI");
+		  driver.findElement(By.xpath(readProperty("month"))).sendKeys("October");
+		  driver.findElement(By.xpath(readProperty("year"))).sendKeys("2020");
+		  driver.findElement(By.xpath(readProperty("purchase"))).click();
+		  
+	  }
+	  
+	  @Test(priority = 6)
+	  public void purchaseDetails() throws InterruptedException {
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(readProperty("purchase_details")))); 
+		  String purchase_detail = driver.findElement(By.xpath(readProperty("purchase_details"))).getText();
+		  String details[] = purchase_detail.split("\\n");
+		  for(String detail:details){
+			  if(detail.contains("Id")){
+				  String id[] = detail.split(" ");
+				  System.out.println("id of order is " + id[id.length-1]);
+			  }
+			  if(detail.contains("Amount")){
+				  String amount[] = detail.split(" ");
+				  System.out.println("Amount of order is " + amount[amount.length-2] + " USD");
+				  Assert.assertEquals(Integer.parseInt(amount[amount.length-2]), 790, "Purchase amount is not as per expected.");
+			  }
+			  
+		  }
+		  driver.findElement(By.xpath(readProperty("ok"))).click();
+	  }
+	  
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit();
+	  }
+
+}
+
+
+
+
+
+
+
---
+++ Assignments/DemoPetStore/code/DemoPetStore.java
@@ -0,0 +1,136 @@
+package com.qa.pet;
+
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+public class DemoPetStore {
+	int id;
+	String url;
+	RequestSpecification httprequest;
+	Response response;
+	
+	public DemoPetStore(String url, int id){
+		
+		this.id = id;
+		this.url = url;	
+		RestAssured.baseURI = url;
+		httprequest = RestAssured.given();
+		
+	}
+	
+	public JSONObject createPetData(String cat_name, String main_name, String photo_url, String status) {
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("id", id);
+	    
+	    Map<String, Object> category = new LinkedHashMap<String, Object>(); 
+	    category.put("id",id);
+	    category.put("name",cat_name);
+		requestParams.put("category",category);
+		
+		requestParams.put("name", main_name);
+		
+		ArrayList<String> photo_urls = new ArrayList<String>();
+		photo_urls.add(photo_url);
+		requestParams.put("photoUrls", photo_urls);
+		
+		ArrayList<JSONObject> tags_array = new ArrayList<JSONObject>();
+		JSONObject tags = new JSONObject();
+		tags.put("id", id);
+		tags.put("name", "string");
+		tags_array.add(tags);
+		requestParams.put("tags", tags_array);
+		
+		requestParams.put("status", status);
+		return requestParams;
+		
+	}
+	
+	public void getAvailablePets(String get_parameter){
+		response = httprequest.request(Method.GET,get_parameter);
+		String responseBody = response.getBody().asString();
+		JSONArray array = new JSONArray(responseBody);
+		for(int i=0; i < array.length(); i++) {
+	         JSONObject object = array.getJSONObject(i);
+	         Assert.assertEquals(object.getString("status"), "available", "Status is not availabe");
+	      }
+		int status_code = response.getStatusCode();
+		if(status_code == 200) {
+			System.out.println("Information of pets with status availabe has been fectched successfully with response code: " + status_code);
+		}
+		
+		}
+	
+	public void postAvailabePet(String post_parameter, String cat_name, String main_name, String photo_url, String old_status) {	
+		JSONObject object = createPetData(cat_name, main_name, photo_url, old_status);
+		httprequest.header("Content-Type", "application/json");
+		httprequest.body(object.toString());
+		Response response = httprequest.request(Method.POST, post_parameter);
+		int status_code = response.getStatusCode();
+		Assert.assertEquals(status_code, 200, "Failed to Post new Pet.");
+		System.out.println("New Pet with status as available has been posted successfully with response code is " + status_code);
+		}
+	
+	public void updateStatus(String update_parameter, String cat_name, String main_name, String photo_url, String new_status) {
+		response = httprequest.request(Method.GET, update_parameter + "/" + id);
+		if(response.getStatusCode() == 200) {
+			System.out.println("Pet is avaliable with id: "+ id);
+			JSONObject object = createPetData(cat_name, main_name, photo_url, new_status);
+			httprequest.header("Content-Type", "application/json");
+	 		httprequest.body(object.toString());
+	 		Response response = httprequest.request(Method.PUT,update_parameter);
+	 		int status_code = response.getStatusCode();
+	 		Assert.assertEquals(status_code, 200, "Status is not updated for given id pet." + id);
+	 		System.out.println("Status of pet with id " + id + " has been updated as " + new_status + " with response code is " + status_code);
+		}else {
+			System.out.println("Oh! Pet is not avaliable with id: "+ id);
+		}
+		
+		
+				}
+	        
+	public void deletePet(String delete_parameter) {
+		response = httprequest.request(Method.GET,delete_parameter);
+		if(response.getStatusCode() == 200) {
+			System.out.println("Pet is avaliable with id: "+ id);
+			response = httprequest.request(Method.DELETE, delete_parameter);
+			int status_code = response.getStatusCode();
+			Assert.assertEquals(status_code, 200, "Pet is not deleted with: " + id);
+			System.out.println("Pet has been deleted successfully with response code is: " + status_code);
+		}else {
+			System.out.println("Oh! Pet is not avaliable with id: "+ id);
+		}
+		
+	}
+		
+	
+
+	public static void main(String[] args) {
+		int id = 110011111;
+		String base_url = "https://petstore.swagger.io";
+		String parameter = "/v2/pet";
+		String get_parameter = parameter + "/findByStatus?status=available";
+		String post_parameter= parameter;
+		String update_parameter = parameter;
+		String delete_parameter = parameter + "/" +id;
+		String cat_name = "string";
+		String main_name = "doggie";
+		String photo_url = "string";
+		String old_status = "available";
+		String new_status = "sold";
+		DemoPetStore petObj = new DemoPetStore(base_url, id);
+		petObj.getAvailablePets(get_parameter);
+		petObj.postAvailabePet(post_parameter, cat_name, main_name, photo_url, old_status);
+		petObj.updateStatus(update_parameter, cat_name, main_name, photo_url, new_status);
+		petObj.deletePet(delete_parameter);
+	}
+
+}
