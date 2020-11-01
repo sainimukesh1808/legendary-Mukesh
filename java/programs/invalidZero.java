243ca59cda5d9c426ea65aadfe5e9ba55d2f5a69
sainimukesh1808 <49982074+sainimukesh1808@users.noreply.github.com>
2020-11-01 20:46:12 +0530

    Update invalidZero.java

--- java/programs/invalidZero.java
+++ java/programs/invalidZero.java
@@ -1,4 +1,16 @@
+/*
+Consider 0 as an invalid number. if two consecutive numbers are same then convert left number to zero and right to double of it's value. 
+In the end, shifts all zeros to right.
+I/P: 2,4,5,0,0,5,5,4,8,6,0,6,8,8
+	2,4,0,0,0,10,5,4,8,6,0,6,8,8
+	2,4,0,0,0,10,5,4,8,0,0,12,8,8
+	2,4,0,0,0,10,5,4,8,0,0,12,0,16
+O/P: 2,4,10,5,4,8,12,16,0,0,0,0,0,0
+*/
+
 package com.qa.java;
+
+import java.util.Arrays;
 
 public class invalidZero {
 		public static void main (String[] args) {
@@ -25,9 +37,11 @@ public class invalidZero {
 	        }}
 	        
 	    }
-	    for(int s_ele:arr){
-	      System.out.print(s_ele);
-	    }
+	    System.out.println("New Araay: " + Arrays.toString(arr));
 		
 		}
 	}
+
+/*
+O/P: New Array: [2, 4, 10, 5, 4, 8, 12, 16, 0, 0, 0, 0, 0, 0]
+*/
